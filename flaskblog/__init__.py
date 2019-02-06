244401c97af09ff7f08f4bbf7fa3ad018f21d9c9
tonuidavies <tonuidavies@gmail.com>
2019-02-06 16:31:23 +0300

    Reset password is now fully working. The token has been set to 30mins. Quite exciting how it sends emails

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,18 +1,24 @@
+
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_migrate import Migrate
-from flask_login import  LoginManager
-
+from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-migrate = Migrate(app, db)
-login_manager= LoginManager(app)
+login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'tonuidavies@gmail.com'
+app.config['MAIL_PASSWORD'] = 'headmaster00'
+mail = Mail(app)
 
 from flaskblog import routes
