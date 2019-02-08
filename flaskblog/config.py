5dcc9b872d4a6097a10cc1546e5fbc543b39ca4f
tonuidavies <tonuidavies@gmail.com>
2019-02-08 20:14:01 +0300

    Update config.py
    Make sure you use enviroment variable without them being hard coded on the script.

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    SECRET_KEY = 'zyuytdsrrhdcythgxhgchvt'
-    SQLALCHEMY_DATABASE_URI ='sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'tonuidavies@gmail.com'
-    MAIL_PASSWORD = 'headmaster00'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
